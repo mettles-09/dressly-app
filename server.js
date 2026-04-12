0adceb158bff67f479ff80227da72b08789bbd89
Shivam <shivammittal0307200@gmail.com>
2026-04-12 05:54:46 +0530

    adding features and making ios app

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ const server = http.createServer(async (req, res) => {
   // Static files
   let filePath = req.url.split('?')[0];
   if (filePath === '/') filePath = '/index.html';
-  filePath = path.join(__dirname, filePath);
+  filePath = path.join(__dirname, 'www', filePath);
   fs.readFile(filePath, (err, content) => {
     if (err) { res.writeHead(404); res.end('Not found'); return; }
     res.writeHead(200, { 'Content-Type': MIME[path.extname(filePath)] || 'application/octet-stream' });
